33a77febee5d2ce03c1f5a9cdf42d8d6b5e4f2b0
austinthale <bbqdpickles@gmail.com>
2018-11-28 11:59:29 -0700

    Austin's Camera Work

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraManager : MonoBehaviour {
+
+    public GameObject GridManage;
+    public GameObject CameraPanel;
+    public GameObject Camera_Image;
+    public List<Camera> cameraList;
+    int index = 0;
+
+
+    // Use this for initialization
+    void Start () {
+		//foreach()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void nextClicked()
+    {
+        if (index < cameraList.Count)
+        {
+            //Camera_Image.
+            index++;
+        }
+        else
+        {
+            index = 0;
+        }
+    }
+}
