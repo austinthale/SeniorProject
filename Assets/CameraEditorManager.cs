69e6a653b096d3efb105e58cc3c11cafb3e3db88
Jacob Ressler <jacobrressler@gmail.com>
2018-12-04 15:53:31 -0700

    Adding editor stuff beginings and placement test scene

---
+++ Assets/CameraEditorManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEditorManager : EditorManager {
+
+    private List<GameObject> camera_list;
+    public GameObject camera_prefab;
+    private int index = 0;
+
+    public override void Add()
+    {
+        if (camera_prefab != null)
+        {
+            camera_list.Add(Instantiate(camera_prefab));
+        }
+        else
+        {
+            print("camer_prefab is null:::CameraEditorManager--Add()");
+        }
+        throw new NotImplementedException();
+    }
+
+    public override void Delete(int ind)
+    {
+        camera_list.Remove(camera_list[ind].gameObject);
+        throw new NotImplementedException();
+    }
+
+    public override void Move()
+    {
+        
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/EditorManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EditorManager : MonoBehaviour {
+
+    public abstract void Add();
+    public abstract void Move();
+    public abstract void Delete(int ind);
+    private string _name;
+}
